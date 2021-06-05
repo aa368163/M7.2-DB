0078264cb9e0e72b22ffccf13853128286370ae5
aa368163 <aa368163@gmail.com>
2021-06-05 10:05:05 +0800

    fix_test

--- app/src/test/java/main/DiscountTest.java
+++ app/src/test/java/main/DiscountTest.java
@@ -10,14 +10,14 @@ public class DiscountTest {
 		String dateTime = "2021-05-26 週三 14:30:00";
 
 		@Test
-		public void testAgeHasDiscount() throws Throwable {
+		void testAgeHasDiscount() throws Throwable {
 			Identity identity = new Identity(10, false, false);
 			Discount discount = new Discount(identity, dateTime);
 			Assertions.assertEquals(0.8, discount.getDiscount());
 		}
 
 		@Test
-		public void testAgeLessThan() throws Throwable {
+		void testAgeLessThan() throws Throwable {
 			Identity identity = new Identity(2, false, true);
 			try {
 				new Discount(identity, dateTime);
@@ -27,7 +27,7 @@ public class DiscountTest {
 		}
 
 		@Test
-		public void testAgeMoreThan() throws Throwable {
+		void testAgeMoreThan() throws Throwable {
 			Identity identity = new Identity(88, true, false);
 			try {
 				new Discount(identity, dateTime);
@@ -37,7 +37,7 @@ public class DiscountTest {
 		}
 
 		@Test
-		public void testAgeHasDiscount_2() throws Throwable {
+		void testAgeHasDiscount_2() throws Throwable {
 			Identity identity = new Identity(70, false, false);
 			Discount discount = new Discount(identity, dateTime);
 			Assertions.assertEquals(0.8, discount.getDiscount());
@@ -49,21 +49,21 @@ public class DiscountTest {
 	@Nested
 	class DifferentBusinessHours {
 		@Test
-		public void testEarlyBirdBusiness() throws Throwable {
+		void testEarlyBirdBusiness() throws Throwable {
 			Identity identity = new Identity(25, false, false);
 			Discount discount = new Discount(identity, "2021-05-26 週三 06:30:00");
 			Assertions.assertEquals(0.8, discount.getDiscount());
 		}
 
 		@Test
-		public void testHaveBusiness() throws Throwable {
+		void testHaveBusiness() throws Throwable {
 			Identity identity = new Identity(25, false, false);
 			Discount discount = new Discount(identity, "2021-05-26 週三 10:30:00");
 			Assertions.assertEquals(1, discount.getDiscount());
 		}
 
 		@Test
-		public void testNoBusiness() throws Throwable {
+		void testNoBusiness() throws Throwable {
 			Identity identity = new Identity(25, false, false);
 			try {
 				Discount discount = new Discount(identity, "2021-05-26 週三 04:30:00");
@@ -73,7 +73,7 @@ public class DiscountTest {
 		}
 
 		@Test
-		public void testNoBusiness_2() throws Throwable {
+		void testNoBusiness_2() throws Throwable {
 			Identity identity = new Identity(25, false, false);
 			try {
 				Discount discount = new Discount(identity, "2021-05-26 週三 22:10:00");
@@ -83,7 +83,7 @@ public class DiscountTest {
 		}
 
 		@Test
-		public void testNoBusiness_3() throws Throwable {
+		void testNoBusiness_3() throws Throwable {
 			Identity identity = new Identity(25, false, false);
 			try {
 				Discount discount = new Discount(identity, "2021-05-26 週三 23:10:00");
@@ -100,14 +100,14 @@ public class DiscountTest {
 		String dateTime = "2021-05-26 週三 14:30:00";
 
 		@Test
-		public void testIsMember() throws Throwable {
+		void testIsMember() throws Throwable {
 			Identity identity = new Identity(25, true, false);
 			Discount discount = new Discount(identity, dateTime);
 			Assertions.assertEquals(0.5, discount.getDiscount());
 		}
 
 		@Test
-		public void testIsGroup() throws Throwable {
+		void testIsGroup() throws Throwable {
 			Identity identity = new Identity(25, false, true);
 			Discount discount = new Discount(identity, dateTime);
 			Assertions.assertEquals(0.7, discount.getDiscount());
@@ -120,7 +120,7 @@ public class DiscountTest {
 		String dateTime = "2021-05-26 週三 14:30:00";
 
 		@Test
-		public void testSet() throws Throwable {
+		void testSet() throws Throwable {
 			Identity identity = new Identity(25, true, false);
 			identity.setAge(30);
 			identity.setMember(false);
@@ -134,7 +134,7 @@ public class DiscountTest {
 	@Nested
 	class Money {
 		@Test
-		public void testMoney() throws Throwable {
+		void testMoney() throws Throwable {
 			Identity identity = new Identity(25, true, false);
 			Discount discount = new Discount(identity, "2021-05-26 週三 10:30:00");
 			Payment payment = new Payment(discount, "2021-05-26 週三 10:30:00");
@@ -142,7 +142,7 @@ public class DiscountTest {
 		}
 
 		@Test
-		public void testMoney_2() throws Throwable {
+		void testMoney_2() throws Throwable {
 			Identity identity = new Identity(25, false, false);
 			Discount discount = new Discount(identity, "2021-05-29 週六 10:30:00");
 			Payment payment = new Payment(discount, "2021-05-29 週六 10:30:00");
