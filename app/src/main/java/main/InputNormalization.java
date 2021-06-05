632358fe3b8bc685b173189d1fbd118c9d8d804f
aa368163 <aa368163@gmail.com>
2021-06-05 11:11:28 +0800

    Update InputNormalization.java

--- app/src/main/java/main/InputNormalization.java
+++ app/src/main/java/main/InputNormalization.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class InputNormalization {
@@ -45,6 +43,7 @@ public class InputNormalization {
                     }
                 } catch (IOException exception) {
                     System.out.println(error);
+                    logger.log(error);         
                     method.invoke(null);
                 }
                 break;
